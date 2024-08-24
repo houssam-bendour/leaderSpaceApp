4d44a4ea4ececcd48d6d598efecd380ab3c9e2a7
SihamEl23 <122886637+SihamEl23@users.noreply.github.com>
2024-08-24 16:05:53 +0100

    add style to login page

--- LeaderSpaceV2/src/main/java/com/management/leaderspace/config/securityConfig.java
+++ LeaderSpaceV2/src/main/java/com/management/leaderspace/config/securityConfig.java
@@ -41,7 +41,7 @@ public class securityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/login", "/error","/forgot-password","/reset-password").permitAll()
+                                .requestMatchers("/login", "/error","/forgot-password","/reset-password","/static/css/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .formLogin(formLogin ->
