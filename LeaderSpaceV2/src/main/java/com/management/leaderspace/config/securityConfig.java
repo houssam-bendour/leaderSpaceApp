190dab415e996afc8aba34eb890243715fcce5d7
zakariae touache <zakariaetouache@gmail.com>
2024-08-31 11:04:39 +0000

    responsive change mot de passe

--- LeaderSpaceV2/src/main/java/com/management/leaderspace/config/securityConfig.java
+++ LeaderSpaceV2/src/main/java/com/management/leaderspace/config/securityConfig.java
@@ -41,7 +41,7 @@ public class securityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/login", "/error","/forgot-password","/reset-password").permitAll()
+                                .requestMatchers("/image/**","/login", "/error","/forgot-password","/reset-password").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .formLogin(formLogin ->
